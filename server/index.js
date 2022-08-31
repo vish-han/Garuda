b64341efe4629abf0ce7c5876c7dff948e24df0d
VasuDevrani <vasu2002devrani@gmail.com>
2022-08-31 13:17:41 +0530

    added controllers

--- server/index.js
+++ server/index.js
@@ -4,20 +4,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
-const mongoose = require('mongoose');
 const PORT=8000;
-app.get("/",(req, res) => {
-    res.send("hello world!");
-})
 
 mongoose.connect("mongodb+srv://vishaldev09:vishal1234@cluster0.npdpway.mongodb.net/?retryWrites=true&w=majority")
 .then(console.log('Connected to MongoDB Successfully'))
 .catch(console.error)
-
-app.listen(PORT,()=>{
-    console.log(`listening on port${PORT}`);
-}); 
-
 
 // routes here
 const EventRoute = require("./routes/EventRoutes");
@@ -36,4 +27,6 @@ app.get("*", (req, res) => {
   res.status(404).send("PAGE NOT FOUND");
 });
 
-
+app.listen(PORT,()=>{
+  console.log(`listening on port ${PORT}`);
+}); 
