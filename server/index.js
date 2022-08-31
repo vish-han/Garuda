dc6f88c7d67f3d51b9e0d117b499d507509756dd
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-08-31 12:59:28 +0530

    Dependencies Added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
-
+app.use(cors());
 const PORT=8000;
 app.get("/",(req, res) => {
     res.send("hello world!");
