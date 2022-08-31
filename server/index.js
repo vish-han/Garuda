ff2d7a5e31ef00599aff5a182b89b8df7a3c57b6
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-08-31 11:42:41 +0530

    MongoDB Connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 const PORT=8000;
 app.get("/",(req, res) => {
     res.send("hello world!");
 })
+
+mongoose.connect("mongodb+srv://vishaldev09:vishal1234@cluster0.npdpway.mongodb.net/?retryWrites=true&w=majority")
+.then(console.log('Connected to MongoDB Successfully'))
+.catch(console.error)
 
 app.listen(PORT,()=>{
     console.log(`listening on port${PORT}`);
