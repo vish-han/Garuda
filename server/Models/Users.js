240dde05e92f22e09d84509d9c5f31ce9a0d6a4a
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-09-01 12:07:19 +0530

    All APIs Created

---
+++ server/Models/Users.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const{Schema} = mongoose
+
+const UserSchema = new Schema({
+    name:{type:"string",require:true},
+    password:{type:"string",require:true},
+    college:{type:"string",require:true},
+    date: { required: true, type: Date, default: Date.now },
+    email:{type:"string",require:true}
+
+})
+const User = mongoose.model("User", UserSchema);
+module.exports = User;  
--- server/index.js
+++ server/index.js
@@ -2,18 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
 const app = express();
-const mongoose = require('mongoose');
+
 const PORT=8000;
-app.get("/",(req, res) => {
-    res.send("hello world!");
-})
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+const UserRoute = require("./Routes/UserRoutes.js");
+app.use('/',UserRoute);
 
 mongoose.connect("mongodb+srv://vishaldev09:vishal1234@cluster0.npdpway.mongodb.net/?retryWrites=true&w=majority")
 .then(console.log('Connected to MongoDB Successfully'))
 .catch(console.error)
 
+app.get("/",(req, res) => {
+  res.send("hello world!");
+})
 app.listen(PORT,()=>{
     console.log(`listening on port${PORT}`);
 }); 
@@ -22,11 +29,6 @@ app.listen(PORT,()=>{
 // routes here
 
 // middlewares
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
 
 
 app.get("*", (req, res) => {
