cb1838873f0c4f0679a8315aaf84e74993b98d50
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-09-03 23:44:47 +0530

    Student and organizer Checkbox added

--- client/src/pages/Organizer.jsx
+++ client/src/pages/Organizer.jsx
@@ -14,7 +14,7 @@ export default function Organizer() {
 
   const [query, setQuery] = useState("");
   const [location, setLocation] = useState("");
-  const [bottom, setBottom] = useState(false);
+  const [bottom,  setBottom] = useState(false);
 
   const toggleDrawer = () => {
       setBottom(!bottom);
